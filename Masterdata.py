8701dd305189098fabc7ed67897accbf2bc77597
sudheerSk215 <sudheer.sk215@gmail.com>
2024-12-29 19:27:29 +0530

    Update Masterdata.py

--- Masterdata.py
+++ Masterdata.py
@@ -1,4 +1,21 @@
 from pyspark.sql import sparksession
+from pyspark import sparkconf, sparkcontext
+from pyspark.sql.functions import *
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages io.delta:delta-spark_2.12:3.2.0 pyspark-shell'
+os.environ['JAVA_HOME'] = r'c:\programFiles\java\jdk1.8.0_202
+
+conf = sparkconf().setAppName("pyspark").setMaster("local[*]")
+sc = sparkcontext(conf = conf)
+
+
+spark = ( sparksession.builder
+                      .appName("Masterdata")
+                      .getOrCreate()
+        )
+
 
 deltadf = spark.read.format("parquet").load("s3://skbuck"/dest/deltadata")
 snowdf = spark.read.load("s3://skbuck/dest/site_count")
